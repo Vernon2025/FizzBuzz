89372fd962b80ccf7793698926df106674ebcb46
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-10-19 11:29:56 -0700

    The start of project

---
+++ fizzbuzz.java
@@ -0,0 +1,16 @@
+public class fizzbuzz
+{
+
+    
+    
+    public void run() {
+        System.out.println("This is the FizzBuzz project");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        fizzbuzz fb = new fizzbuzz();
+        fb.run();
+    }
+}
