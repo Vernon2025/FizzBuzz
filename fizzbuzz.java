0c5db9230e791f47fe85051b1aa7638d429b6a6c
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-10 11:33:21 -0800

    Test for program

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -6,10 +6,22 @@ public class fizzbuzz
     public void run() {
         String Fizz = "Fizz";
         String Buzz = "Buzz";
-        String FizzBuzz = "FizzBuzz";
         String str = "";
+        
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
+            if (i == 2)
+                System.out.print(Fizz);              
+            if (i == 4)
+                System.out.print(Buzz);
+            if (i == 5)
+                System.out.print(Fizz);
+            if (i == 8)
+                System.out.print(Fizz);
+            if (i == 9)
+                System.out.print(Buzz);
+            if (i == 11) 
+                System.out.print(Fizz);
         }
     }
     
