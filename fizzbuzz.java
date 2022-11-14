8b608a00de2ce03457f3451fddf0fb51a6ac7e7d
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-13 20:03:18 -0800

    FizzBuzz Complete

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,31 +1,43 @@
 public class fizzbuzz
 {
-
-    
-    
     public void run() {
-        String Fizz = "Fizz";
-        String Buzz = "Buzz";
-        String str = "";
-        
+        int fizz = 3;  
+        int buzz = 5;  
+        int fizzbuzz = 15; 
+        boolean isFizz;
+        boolean isBuzz;
+        boolean isFizzBuzz;
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
-            if (i == 2)
-                System.out.print(Fizz);              
-            if (i == 4)
-                System.out.print(Buzz);
-            if (i == 5)
-                System.out.print(Fizz);
-            if (i == 8)
-                System.out.print(Fizz);
-            if (i == 9)
-                System.out.print(Buzz);
-            if (i == 11) 
-                System.out.print(Fizz);
+            isFizz = false;
+            isBuzz = false;
+            isFizzBuzz = false;
+            
+
+            if (i == fizz) {       
+                isFizz = true;      
+                fizz += 3;           
+            }
+            if (i == buzz) {
+                isBuzz = true;
+                buzz += 5;
+            }
+            if (i == fizzbuzz) {
+                isFizzBuzz = true;
+                fizzbuzz += 15;
+            }
+            
+             
+            if (isFizz && !isFizzBuzz) {
+                System.out.println("Fizz");
+            } else if (isBuzz && !isFizzBuzz) {
+                System.out.println("Buzz");
+            } else if (isFizzBuzz) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
-    
-    
     public static void main(String[] args)
     {        
         fizzbuzz fb = new fizzbuzz();
